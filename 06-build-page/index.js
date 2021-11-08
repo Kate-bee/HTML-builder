1a33b2910f6ebb8350e5d097596e0ab4aa2dd6ca
Kate-bee <kate280987@gmail.com>
2021-11-09 01:47:21 +0300

    fix: delete a comment

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -16,23 +16,7 @@ fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, array)
         }
     });
 });
-/*const sourceAssets = path.join(__dirname, "./assets");
-const destinationAssets = path.join(destinationDir, "./assets");
-fs.mkdir(destinationAssets, { recursive: true }, () => {//recursive: false - в этом случае вызов, fs.mkdir()когда path является существующим каталогом, приводит к ошибке.
-});
-fs.readdir(sourceAssets, { withFileTypes: true }, (err, array) => {//когда fs.readdir()или fs.readdirSync()вызывается с withFileTypesпараметром, установленным в true, результирующий массив заполняется объектами <fs.Dirent>
-    if (err) throw err;
-    array.forEach((el) => {
-        let sourcePath = path.join(sourceAssets, el.name);
-        let destinationPath = path.join(destinationAssets, el.name);
-        if (el.isDirectory()) {
-            fs.mkdir(destinationPath, { recursive: true });
-            copyFiles(sourcePath, destinationPath);
-        } else {
-            fs.copyFile(sourcePath, destinationPath, () => { });
-        }
-    })
-})*/
+
 const sourceAssets = path.join(__dirname, 'assets');
 const copy = async (source, destination) => {
     await fs.mkdir(destination, { recursive: true }, () => { });
