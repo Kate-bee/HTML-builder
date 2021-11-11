68e3fb209d806e1d9157d3451c0238cd366b11ea
Kate-bee <kate280987@gmail.com>
2021-11-11 08:51:17 +0300

    fix: change the task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,11 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const sourceDir = path.join(__dirname, "./files");
 const destinationDir = path.join(__dirname, "./files-copy");
-fs.mkdir(destinationDir, { recursive: true }, () => {//recursive: false - в этом случае вызов, fs.mkdir()когда path является существующим каталогом, приводит к ошибке.
-});
-fs.readdir(sourceDir, { withFileTypes: true }, (err, array) => {//когда fs.readdir()или fs.readdirSync()вызывается с withFileTypesпараметром, установленным в true, результирующий массив заполняется объектами <fs.Dirent>
-    if (err) throw err;
-    array.forEach((el) => {
-        fs.copyFile(path.join(sourceDir, el.name), path.join(destinationDir, el.name), () => { });
-    })
-})
+async function createFolder() {
+    await fs.rmdir(destinationDir, { recursive: true, force: true }, err => {
+        if (err) throw err;
+        fillFolder();
+    });
+    await fs.mkdir(destinationDir, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+}
+async function fillFolder() {
+    await fs.mkdir(destinationDir, { recursive: true }, err => {
+        if (err) throw err;
+    });
+    await fs.readdir(sourceDir, { withFileTypes: true }, (err, array) => {
+        if (err) throw err;
+        array.forEach(el => {
+            if (el.isFile()) {
+                fs.copyFile(path.join(sourceDir, el.name), path.join(destinationDir, el.name), (err) => {
+                    if (err) throw err;
+                });
+            };
+        });
+    });
+}
+createFolder();
